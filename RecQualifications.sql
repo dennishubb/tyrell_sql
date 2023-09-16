db8306be5458127f084aa7c975cdc98a737293f2
Dennis Wong <dennis@Denniss-MacBook-Air.local>
2023-09-16 18:05:18 +0800

    fix typo

--- RecQualifications.sql
+++ RecQualifications.sql
@@ -3,7 +3,7 @@ use TyrellSystems;
 CREATE TABLE RecQualifications (
     id bigint auto_increment,
     name varchar(255),
-    type tinyint
+    type tinyint,
     deleted tinyint,
     PRIMARY KEY (id)
 );
